2970042a5b13fc351bed04972e8bc128e7920e06
Sultan <sula@Sultans-MacBook-Air.local>
2023-10-23 20:08:48 +0200

    Removed KMM Bridge plugin

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.multiplatform.swiftpackage)
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kmmbridge)
     id("maven-publish")
 }
 
