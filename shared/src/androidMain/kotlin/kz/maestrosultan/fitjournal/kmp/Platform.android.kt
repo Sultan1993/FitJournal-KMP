ee4f231594f799445870e77126e15e273ec6d544
Sultan <sula@Sultans-MacBook-Air.local>
2023-10-31 19:31:30 +0100

    Version 0.1.0
    Added "createdAt" and "updatedAt" fields

--- shared/src/androidMain/kotlin/kz/maestrosultan/fitjournal/kmp/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package kz.maestrosultan.fitjournal.kmp
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
